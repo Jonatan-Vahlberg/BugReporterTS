cc997967b8da08a89ff79f6bddd26ed294c9256c
Jonatan Ramses Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2020-01-30 14:35:18 +0100

    added data models and context to application

--- App.tsx
+++ App.tsx
@@ -1,19 +1,109 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Profile from './src/models/Profile';
+import Team from './src/models/Team';
+import { ApplicationContext } from './src/context/ApplicationContext';
+import BugReport from './src/models/BugReport';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+type AppProps = {};
+
+type AppState = {
+  profile?: Profile;
+  teams?: Team[];
+  featuredTeam?: Team;
+  settings?: object;
+};
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      profile: null,
+      teams: [],
+      featuredTeam: null,
+      settings: {},
+    };
+  }
+
+  render() {
+    return (
+      <ApplicationContext.Provider
+        value={{
+          profile: this.state.profile,
+          teams: this.state.teams,
+          featuredTeam: this.state.featuredTeam,
+          profileActions: {
+            createProfile: this.createProfile,
+            getProfile: this.getProfile,
+            updateProfile: this.updateProfile,
+            login: this.login,
+            logout: this.logout,
+          },
+          teamsActions: {
+            joinTeam: this.joinTeam,
+            leaveTeam: this.leaveTeam,
+            createTeam: this.createTeam,
+          },
+          teamActions: {
+            createReport: this.createReport,
+            updateReport: this.updateReport,
+            viewReport: this.viewReport,
+            requestAddTeamMember: this.requestAddTeamMember,
+          },
+          settings: this.state.settings,
+        }}
+      >
+        {null}
+      </ApplicationContext.Provider>
+    );
+  }
+
+  //* PROFILE ACTIONS
+  createProfile = async (profile: Profile) => {
+    this.setState({ profile });
+    //TODO: firebase create profile
+  };
+  getProfile = async (uuid: string) => {
+    //TODO: firebase getProfile
+  };
+  updateProfile = async (updatedProfile: Profile) => {
+    this.setState({ profile: updatedProfile });
+    //TODO: firebase update profile
+  };
+  login = async (email: string, password: string) => {
+    //TODO: firebase LOGIN
+  };
+  logout = async () => {
+    //TODO: firebase LOGOUT
+  };
+
+  //*TEAMS ACTIONS
+  joinTeam = async (code: string) => {
+    //TODO: firebase LOGOUT
+  };
+  leaveTeam = async () => {
+    //TODO: firebase LOGOUT
+  };
+  createTeam = async (team: Team) => {
+    //TODO: firebase LOGOUT
+  };
+
+  //* TEAM ACTIONS
+  createReport = async (bugReport: BugReport) => {
+    //TODO: firebase Add report
+  };
+
+  updateReport = async (bugReport: BugReport) => {
+    //TODO: firebase Add report
+  };
+
+  viewReport = async (bugReport: BugReport) => {
+    //TODO: firebase Add report
+  };
+  requestAddTeamMember = async (
+    identifier: string,
+    typeOfidentifier: string,
+  ) => {
+    //TODO: firebase Add report
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/models/BugReport.ts
@@ -0,0 +1,23 @@
+import TeamMember from './TeamMember';
+
+export default interface BugReport {
+  uuid: string;
+  title: string;
+  content: string; //for now
+  severity: SeverityValue;
+  reportDate: string; //ISODATESTRING
+  assignedTo?: TeamMember;
+  labels?: string[];
+  dueDate?: string; //ISODATESTRING
+  comments?: Comment[];
+  closed: boolean;
+  reportedBy: string;
+}
+
+export enum SeverityValue {
+  NONE = 0,
+  LOW = 1,
+  NORMAL = 2,
+  HIGH = 4,
+  CATASTROPHIC = 10,
+}
